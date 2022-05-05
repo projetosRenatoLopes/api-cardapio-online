a0adbfcbdf6d76ce9db4e69a408694a5b0661e99
projetosRenatoLopes <tirenatolopes@gmail.com>
2022-05-05 11:13:32 -0300

    restart

--- src/db/db.js
+++
@@ -1,7 +0,0 @@
-import { Pool } from 'pg';
-
-const connectionString = 'postgres://gdtiwcjl:vpD7PrBmK8HyzlZbdt8qKU4W9kjDAtba@motty.db.elephantsql.com/gdtiwcjl';
-
-const db = new Pool({ connectionString });
-
-export default db;
