12b09b05def625b3a21eed8e87d17e98c76d5f9f
projetosRenatoLopes <tirenatolopes@gmail.com>
2022-05-15 20:24:12 -0300

    TOKEN OPCIOS

--- src/repositories/opcoes.repository.js
+++ src/repositories/opcoes.repository.js
@@ -4,23 +4,17 @@ const { verifyJWT } = require('../utils/checkToken');
 
 exports.getCategories = async (req, res, next) => {
     try {
-        const token = req.headers.authorization
-        const vToken = verifyJWT(token)
-
-        if (vToken.status === 401) {
-
-            return res.status(401).send(vToken.message)
-
-        } else if (vToken.status === 500) {
-
-            return res.status(500).send(vToken.message)
-
-        } else if (vToken.status === 200) {
+        // const token = req.headers.authorization
+        // const vToken = verifyJWT(token)
+        // if (vToken.status === 401) {
+        //     return res.status(401).send(vToken.message)
+        // } else if (vToken.status === 500) {
+        //     return res.status(500).send(vToken.message)
+        // } else if (vToken.status === 200) {
             const result = await db.query("SELECT name AS desc, id FROM categories ORDER BY name;");
             const queryRes = result.rows;
             return res.status(200).send(queryRes)
-        }
-
+        //}
     } catch (error) {
         return res.status(500).send('ERROR 500');
     }
@@ -28,23 +22,17 @@ exports.getCategories = async (req, res, next) => {
 
 exports.getPaymentModes = async (req, res, next) => {
     try {
-        const token = req.headers.authorization
-        const vToken = verifyJWT(token)
-
-        if (vToken.status === 401) {
-
-            return res.status(401).send(vToken.message)
-
-        } else if (vToken.status === 500) {
-
-            return res.status(500).send(vToken.message)
-
-        } else if (vToken.status === 200) {
+        // const token = req.headers.authorization
+        // const vToken = verifyJWT(token)
+        // if (vToken.status === 401) {
+        //     return res.status(401).send(vToken.message)
+        // } else if (vToken.status === 500) {
+        //     return res.status(500).send(vToken.message)
+        // } else if (vToken.status === 200) {
             const result = await db.query("SELECT id, name AS desc FROM paymentsmodes ORDER BY id;");
             const queryRes = result.rows;
             return res.status(200).send(queryRes)
-        }
-
+        // }
     } catch (error) {
         return res.status(500).send('ERROR 500');
     }
--- src/repositories/product.repository.js
+++ src/repositories/product.repository.js
@@ -5,9 +5,9 @@ exports.findAllProducts = async (req, res, next) => {
     try {
         const result = await db.query("SELECT * FROM products WHERE tagcompany = '" + [req.params.company] + "';");
         const queryRes = result.rows;
-        var response = { products: 'Erro ao consultar produtos' }
+        var response = [{ length: 0, products: ['Erro ao consultar produtos'] }]
         if (queryRes.length === 0) {
-            response = { products: 'Nenhum produto encontrado para ' + [req.params.company] + '.' }
+            response = [{ length: 0, products: ['Nenhum produto encontrado para ' + [req.params.company] + '.'] }]
         } else {
             // @ts-ignore
             response = [{ length: result.rows.length, products: result.rows }]
