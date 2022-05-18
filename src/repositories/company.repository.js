21ea43dcabd4dc36f4731923650805ff88d25fe5
projetosRenatoLopes <tirenatolopes@gmail.com>
2022-05-18 20:00:33 -0300

    color back

--- src/repositories/company.repository.js
+++ src/repositories/company.repository.js
@@ -41,8 +41,9 @@ exports.insertCompany = async (req, res, next) => {
         else if (vToken.status === 200) {
 
             if (checkProps(req.body) === true) {
+
                 //const result = db.query("INSERT INTO company (name, tag, funcdom, funcseg, functer, funcqua, funcqui, funcsex, funcsab, adrrua, adrnum, adrcom, adrbai, adrcid, adrest, txentrega, logo, tel, categs, paymodes) '" + JSON.stringify(req.body.empname) + "','" + [req.body.emptag] + "','" + [req.body.empfuncdom] + "','" + [req.body.empfuncseg] + "','" + [req.body.empfuncter] + "','" + [req.body.empfuncqua] + "','" + [req.body.empfuncqui] + "','" + [req.body.empfuncsex] + "','" + [req.body.empfuncsab] + "','" + [req.body.empadrrua] + "','" + [req.body.empadrnum] + "','" + [req.body.empadrcom] + "','" + [req.body.empadrbai] + "','" + [req.body.empadrcid] + "','" + [req.body.empadrest] + "','" + [req.body.emptxentrega] + "','" + [req.body.emplogo] + "','" + [req.body.emptel] + "','" + [req.body.empcategs] + "','" + [req.body.emppaymodes] + "';");
-                const result = await db.query("INSERT INTO company (name, tag, funcdom, funcseg, functer, funcqua, funcqui, funcsex, funcsab, adrrua, adrnum, adrcom, adrbai, adrcid, adrest, txentrega, logo, tel, categs, paymodes) VALUES ('" + [req.body.empname] + "','" + [req.body.emptag] + "','" + [req.body.empfuncdom] + "','" + [req.body.empfuncseg] + "','" + [req.body.empfuncter] + "','" + [req.body.empfuncqua] + "','" + [req.body.empfuncqui] + "','" + [req.body.empfuncsex] + "','" + [req.body.empfuncsab] + "','" + [req.body.empadrrua] + "','" + [req.body.empadrnum] + "','" + [req.body.empadrcom] + "','" + [req.body.empadrbai] + "','" + [req.body.empadrcid] + "','" + [req.body.empadrest] + "','" + [req.body.emptxentrega] + "','" + [req.body.emplogo] + "','" + [req.body.emptel] + "','" + [req.body.empcategs] + "','" + [req.body.emppaymodes] + "');");
+                const result = await db.query("INSERT INTO company (name, tag, funcdom, funcseg, functer, funcqua, funcqui, funcsex, funcsab, adrrua, adrnum, adrcom, adrbai, adrcid, adrest, txentrega, logo, tel, categs, paymodes) VALUES ('" + [req.body.empname] + "','" + [req.body.emptag] + "','" + [req.body.empfuncdom] + "','" + [req.body.empfuncseg] + "','" + [req.body.empfuncter] + "','" + [req.body.empfuncqua] + "','" + [req.body.empfuncqui] + "','" + [req.body.empfuncsex] + "','" + [req.body.empfuncsab] + "','" + [req.body.empadrrua] + "','" + [req.body.empadrnum] + "','" + [req.body.empadrcom] + "','" + [req.body.empadrbai] + "','" + [req.body.empadrcid] + "','" + [req.body.empadrest] + "','" + [req.body.emptxentrega] + "','" + [req.body.emplogo] + "','" + [req.body.emptel] + "','" + [req.body.empcategs] + "','" + [req.body.emppaymodes] + "','" + [req.body.backcolor] + "');");
                 return res.status(200).send({ "status": 200, "message": "Dados inserido com sucesso" });
             } else {
                 return res.status(404).send({ "status": 404, "message": "Requisição inválida" });
@@ -66,18 +67,16 @@ exports.updateCompany = async (req, res, next) => {
             if (user.rowCount === 0) {
                 return res.status(401).send({ "status": 401, "message": "Usuário inválido." });
             } else {
-                if(user.rows[0].tagpage === req.body.emptag){
+                if (user.rows[0].tagpage === req.body.emptag) {
 
-                    const result = await db.query("UPDATE company SET name = '" + [req.body.empname] + "', funcdom = '" + [req.body.empfuncdom] + "', funcseg = '" + [req.body.empfuncseg] + "', functer = '" + [req.body.empfuncter] + "', funcqua = '" + [req.body.empfuncqua] + "', funcqui = '" + [req.body.empfuncqui] + "', funcsex = '" + [req.body.empfuncsex] + "', funcsab = '" + [req.body.empfuncsab] + "', adrrua = '" + [req.body.empadrrua] + "', adrnum = '" + [req.body.empadrnum] + "', adrcom = '" + [req.body.empadrcom] + "', adrbai = '" + [req.body.empadrbai] + "', adrcid = '" + [req.body.empadrcid] + "', adrest = '" + [req.body.empadrest] + "', txentrega = '" + [req.body.emptxentrega] + "', logo = '" + [req.body.emplogo] + "', tel = '" + [req.body.emptel] + "', categs = '" + [req.body.empcategs] + "', paymodes = '" + [req.body.emppaymodes] + "' WHERE  tag = '" + [req.body.emptag] + "';")
-                    console.log(result)
+                    const result = await db.query("UPDATE company SET name = '" + [req.body.empname] + "', funcdom = '" + [req.body.empfuncdom] + "', funcseg = '" + [req.body.empfuncseg] + "', functer = '" + [req.body.empfuncter] + "', funcqua = '" + [req.body.empfuncqua] + "', funcqui = '" + [req.body.empfuncqui] + "', funcsex = '" + [req.body.empfuncsex] + "', funcsab = '" + [req.body.empfuncsab] + "', adrrua = '" + [req.body.empadrrua] + "', adrnum = '" + [req.body.empadrnum] + "', adrcom = '" + [req.body.empadrcom] + "', adrbai = '" + [req.body.empadrbai] + "', adrcid = '" + [req.body.empadrcid] + "', adrest = '" + [req.body.empadrest] + "', txentrega = '" + [req.body.emptxentrega] + "', logo = '" + [req.body.emplogo] + "', tel = '" + [req.body.emptel] + "', categs = '" + [req.body.empcategs] + "', paymodes = '" + [req.body.emppaymodes] + "', backcolor = '" + [req.body.backcolor] + "' WHERE  tag = '" + [req.body.emptag] + "';")
+                    console.log(req.body.backcolor)
                     return res.status(200).send({ "status": 200, "message": "Dados atualizados com sucesso" });
-                } else{
-                    console.log(user.rows[0].tagpage)
-                    console.log(req.body.emptag)
+                } else {
                     return res.status(401).send({ "status": 401, "message": "Usuário sem permissão no dietório" });
                 }
             }
-            
+
             //if (checkProps(req.body) === true) {
 
             //const result = db.query("UPDATE INTO company (name, tag, funcdom, funcseg, functer, funcqua, funcqui, funcsex, funcsab, adrrua, adrnum, adrcom, adrbai, adrcid, adrest, txentrega, logo, tel, categs, paymodes) '" + JSON.stringify(req.body.empname) + "','" + [req.body.emptag] + "','" + [req.body.empfuncdom] + "','" + [req.body.empfuncseg] + "','" + [req.body.empfuncter] + "','" + [req.body.empfuncqua] + "','" + [req.body.empfuncqui] + "','" + [req.body.empfuncsex] + "','" + [req.body.empfuncsab] + "','" + [req.body.empadrrua] + "','" + [req.body.empadrnum] + "','" + [req.body.empadrcom] + "','" + [req.body.empadrbai] + "','" + [req.body.empadrcid] + "','" + [req.body.empadrest] + "','" + [req.body.emptxentrega] + "','" + [req.body.emplogo] + "','" + [req.body.emptel] + "','" + [req.body.empcategs] + "','" + [req.body.emppaymodes] + "';");
